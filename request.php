fc06b1a9a976099915c5eef2c5a8b15c6677a9bd
Stephanie Jones <stephanie.jones@mymona.uwi.edu>
2018-11-14 01:35:36 +0000

    completed excercise 4

--- request.php
+++ request.php
@@ -2,6 +2,7 @@
 // accept a term (keyword)
 // respond with a value
 $query = $_GET['q'];
+$query2 = $_GET['all'];
 $definition = [
     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
     "bar" => "A place that sells alcholic beverages",
@@ -11,5 +12,47 @@ $definition = [
     "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
+
+if($query2=="true"){
+    
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+<entries>
+    <definition name="definition" author="John">
+     A statement of the exact meaning of a word, especially in a dictionary.
+    </definition>
+    
+    <definition name="bar" author="Bob">
+        A place that sells alcholic beverages.
+    </definition>
+    
+    <definition name="ajax" author="Kim">
+        Technique which involves the use of javascript and xml (or JSON).
+    </definition>
+    
+    <definition name="html" author="Alex">
+        The standard markup language for creating web pages and web applications.
+    </definition>
+    
+    <definition name="css" author="Mohammed">
+        A style sheet language used for describing the presentation of a document written in a markup language.
+    </definition>
+    
+    <definition name="javascript" author="Bobby">
+        A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+    </definition>
+    
+    <definition name="php" author="Rian">
+        A server-side scripting language, and a powerful tool for making dynamic and interactive websites.
+    </definition>
+    
+</entries>';
+
+header('Content-Type: text/xml');
+$xmlOutput = new SimpleXMLElement($xmldata);
+echo  $xmlOutput->asXML();
+} else{
+    
 print "<h3>" . strtoupper($query) . "</h3>";
 print "<p>" . $definition[$query] . "</p>";
+}
+
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const httprequest= new XMLHttpRequest();
 const field = document.getElementById("searchfield")
 const result = document.getElementById("result")
-
+const all = document.getElementById("alldefs")
 
 
 document.getElementById("searchbutton").addEventListener("click", function(){
@@ -17,4 +17,28 @@ document.getElementById("searchbutton").addEventListener("click", function(){
        }
    }
 });
+
+all.addEventListener("click",function() {
+    httprequest.open("GET",`request.php?q=&all=true`,true)
+   httprequest.send()
+   httprequest.onreadystatechange= function(){
+       if (httprequest.readyState=== XMLHttpRequest.DONE){
+         if(httprequest.status===200)  {
+             
+       const response=httprequest.responseXML;
+       const definitions = response.getElementsByTagName('definition');
+       const list = document.createElement('ol');
+       for(let i =0;i<definitions.length;i++){
+            const listItem = document.createElement('li');
+            const title = definitions[i].getAttribute('name');
+            const author = definitions[i].getAttribute('author');
+            const definition = definitions[i].innerHTML;
+            listItem.innerHTML=`<h3>${title}</h3><p>${definition}</p><p>-${author}</p>`;
+            list.appendChild(listItem);
+       }
+       result.appendChild(list)
+         }
+       }
+   }
+})
 
